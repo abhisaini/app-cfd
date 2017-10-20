66706e8a67e97e8703e5ff9c09427cdc1d13faf3
abhisaini <abhisni@iitk.ac.in>
2017-10-21 01:19:18 +0530

    an attempt to make gymkhana app during cfd

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function content(){
+  document.getElementById("content").src = "content.html";}
+
+function snt(){
+  document.getElementById("content").src = "snt/snt.html";}
+
+function fmc(){
+  document.getElementById("content").src = "fmc/fmc.html";}
+function gns(){
+  document.getElementById("content").src = "gns/gns.html";}
+function cult(){
+  document.getElementById("content").src = "cult/cult.html";}
+
+function team(){
+  document.getElementById("content").src = "team.html";}
+
+function idea(){
+  document.getElementById("content").src = "idea.html";}
+
+function notify(){
+  document.getElementById("content").src = "notify.html";}
